fdcbb21a8c257d37ac457789a8c8fc2350eeb713
Will Alicar <will@alicar.me>
2024-08-16 07:11:11 -0700

    set emsc nix store path in flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      emscPath = pkgs.emscripten;
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
@@ -16,6 +17,7 @@
         ];
         shellHook = ''
           export SRC=$(pwd)
+          export EMSC_DEV=${emscPath}/share/emscripten/system
           bash scripts/setup_clangd.sh
           bash scripts/setup_vscode.sh
         '';
--- scripts/setup_clangd.sh
+++ scripts/setup_clangd.sh
@@ -2,9 +2,6 @@
 
 if [ ! -f ".clangd" ]; then
     echo "Creating .clangd"
-    emsc_dir_bin=$(eval "echo \$PATH | tr ':' '\n' | grep emscripten")
-    emsc_dir=$(eval "echo ${emsc_dir_bin%/bin}")
-    inc_cmd="-I$emsc_dir/share/emscripten/system/include"
-    inc_cmd="-I$emsc_dir/share/emscripten/system/include"
+    inc_cmd="-I$EMSC_DEV/include"
     echo -e "CompileFlags:\r  Add: [$inc_cmd]" > ".clangd"
 fi
